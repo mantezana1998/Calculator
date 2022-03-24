c9dfe1cec8a8831a53b5cf25dd3e68216ce5f2c7
mantezana <maximo00antezana@gmail.com>
2022-03-23 17:02:28 -0700

    buttons added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import './App.css'
+
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <div className="app">
+      <div className="calculator">
+        <div className="displayNumbers">
+          <div className="operators">
+            <button>+</button>
+            <button>-</button>
+            <button>*</button>
+            <button>/</button>
+          </div>
+          <div className="numbers">
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>4</button>
+            <button>5</button>
+            <button>5</button>
+            <button>6</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>0</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
